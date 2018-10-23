152967bd4e82ba7a3e53ede4d7a73e6730057845
Carrie Li <carrie.t.li@gmail.com>
2018-10-23 06:32:06 +0000

    Refactored transform_data, completed posts and account balance stat functions

--- fruit-salad.py
+++ fruit-salad.py
@@ -3,7 +3,7 @@
 
 import jsonlines
 import re
-from collections import Counter
+import statistics
 from pprint import pprint
 
 
@@ -13,6 +13,7 @@ class FruitSalad:
     def __init__(self):
 
         self.data = []
+        self.transformed = []
 
     def get_data(self):
         """ Extracts data from JSON Lines file and returns a list of objects, 
@@ -24,6 +25,9 @@ class FruitSalad:
         url = "data/simple-etl.jsonl"
         fields = ['name', 'age', 'isActive', 'favoriteFruit', 'balance', 'posts']
 
+        # If data is not already downloaded, then run below block to download
+        # and parse data; if data is already downloaded, then simply return it
+        # below.
         if not self.data:
 
             try:
@@ -37,75 +41,211 @@ class FruitSalad:
                             if key in obj})
 
             except:
-                print "Error: No data found!"
+                print("Error: No data found!")
 
         return self.data
 
 
     def transform_data(self):
-        """ Makes a sub-dictionary from original data and adds it to a list."""
+        """ Makes a sub-dictionary from original data and adds it to a list.
+
+            Test:
+
+            >>> fruit = FruitSalad()
+            >>> bool(fruit.transform_data())
+            True
+        """
 
         data = self.get_data()
-        result = []
 
-        for d in data:
-            sub = {}
+        # Caching the data: if data has already been transformed, just return
+        # it; otherwise, run through the below block to transform the data.
+        if not self.transformed:
 
-            sub.update({ \
-                'full_name': str(d['name']['first'] + " " + d['name']['last']), \
-                'post_count': len(d['posts']), \
-                'most_common_word_in_posts': self.find_most_common_word(d['posts']), \
-                'age': d['age'], \
-                'is_active': d['isActive'], \
-                'favorite_fruit': str(d['favoriteFruit']), \
-                'balance': float(d['balance'].strip('$').replace(',', ''))
-            })
-            result.append(sub)
+            for d in data:
 
-        return result
+                post_lst = []
+
+                # Loop through list of dictionaries (posts) to find words 
+                # regardless of punctuation and convert them to lowercase.
+                for text in d['posts']:
+                    post = re.findall(r'\w+', text['post'].lower())
+                    post_lst.append(post)
+                    
+                # post_lst is a list of lists of all words from the text in 
+                # every post. Then we flatten the list.
+                flatten = [leaf for tree in post_lst for leaf in tree]
+
+                # Update dictionary with multiple entries at a time
+                sub = {}
+                sub.update({ \
+                    'full_name': str(d['name']['first'] + " " + d['name']['last']), \
+                    'post_count': len(d['posts']), \
+                    'most_common_word_in_posts': self.find_most_common_word(flatten), \
+                    'age': d['age'], \
+                    'is_active': d['isActive'], \
+                    'favorite_fruit': str(d['favoriteFruit']), \
+                    'balance': float(d['balance'].strip('$').replace(',', ''))
+                })
+
+                self.transformed.append(sub)
+
+        return self.transformed
 
 
     def find_most_common_word(self, lst):
-        """ Takes in list of posts and returns the most common word(s) in a list."""
+        """ Takes in list of words and returns the most common word(s) in a list.
+            
+            Test:
+            >>> fruit = FruitSalad()
+            >>> fruit.find_most_common_word( \
+                    [ 'me', 'me', 'me', 'i', 'will', 'succeed', 'succeed'])
+            ['me']
+        """
         
         word_lst = []
         freq = {}
         highest_freq = 0
 
-        # Loop thru all posts regardless of punctuation and puts them in lowercase
-        for text in lst:
-            post = re.findall(r'\w+', text['post'].lower())
- x
         # Create dictionary freq with word: count
-        for word in post:
+        for word in lst:
             if word not in freq:
                 freq[word] = 1
             else:
                 freq[word] += 1
 
-        for count in freq.values():
+        # Iterate through the dict and find the highest frequency.
+        # Find the word where the value matches the highest frequency and
+        # append to word_lst.
+        for word, count in freq.items():
             if count > highest_freq:
+                word_lst = [] # Make sure list is completely empty.
+                word_lst.append(str(word))
                 highest_freq = count
 
-        for word in freq.keys():
-            if freq[word] == highest_freq:
+            # If count is the same, add the word to existing word_lst
+            elif count == highest_freq:
                 word_lst.append(str(word))
 
         return word_lst
 
 
-fruit = FruitSalad()
-print(fruit.transform_data())
+    def get_total_posts(self):
+        """ Returns total number of posts in the dataset.
+            
+            Test:
 
-# current data set:
- # {'age': 20,
- #  'balance': u'$3,317.36',
- #  'favoriteFruit': u'strawberry',
- #  'isActive': True,
- #  'name': {u'first': u'Fox', u'last': u'Cummings'}},
- # {'age': 38,
- #  'balance': u'$1,939.57',
- #  'favoriteFruit': u'banana',
- #  'isActive': False,
- #  'name': {u'first': u'Marilyn', u'last': u'Sweeney'}},
+            >>> fruit = FruitSalad()
+            >>> type(fruit.get_total_posts())
+            <class 'int'>
+        """
+
+        data = self.transform_data()
+        total_posts = 0
+
+        for user in data:
+            total_posts += user['post_count']
+
+        return total_posts
+
+
+    def get_mc_overall_word(self):
+        """ Returns most common of the most common words in the dataset."""
+
+        data = self.transform_data()
+        words = []
+
+        for user in data:
+            words.append(user['most_common_word_in_posts'])
+
+        flatten = [leaf for tree in words for leaf in tree]
+
+        return self.find_most_common_word(flatten)
+
+
+    def get_total_bal(self):
+
+        data = self.transform_data()
+        total = 0
+
+        for user in data:
+            total += user['balance']
+
+        # Convert float to string of 2 decimal places, then convert back to a
+        # float.
+        return float(format(total, '.2f'))
+
+
+    def get_mean_bal(self):
+
+        data = self.transform_data()
+
+        bal_lst = []
+
+        for user in data:
+            bal_lst.append(user['balance'])
+
+        return float(format(statistics.mean(bal_lst), '.2f'))
+
+
+    def get_active_mean(self):
+
+        data = self.transform_data()
+
+        active_bal = []
+
+        for user in data:
+            if user['is_active']:
+                active_bal.append(user['balance'])
+
+        return float(format(statistics.mean(active_bal), '.2f'))
+
+
+    def get_strawberry_mean(self):
+
+        data = self.transform_data()
+
+        strawberry = []
+
+        for user in data:
+            if user['favorite_fruit'] == 'strawberry':
+                strawberry.append(user['balance'])
+
+        return float(format(statistics.mean(strawberry), '.2f'))
+
+
+# transform data:
+#  {'age': 20,
+#   'balance': 3317.36,
+#   'favorite_fruit': 'strawberry',
+#   'full_name': 'Fox Cummings',
+#   'is_active': True,
+#   'most_common_word_in_posts': ['in', 'minim'],
+#   'post_count': 10},
+#  {'age': 38,
+#   'balance': 1939.57,
+#   'favorite_fruit': 'banana',
+#   'full_name': 'Marilyn Sweeney',
+#   'is_active': False,
+#   'most_common_word_in_posts': ['elit'],
+#   'post_count': 10},
+
+
+if __name__ == "__main__":
+    
+    import doctest
+
+    fruit = FruitSalad()
+    pprint(fruit.transform_data())
+    print(fruit.get_total_posts())
+    print(fruit.get_mc_overall_word())
+    print(fruit.get_total_bal())
+    print(fruit.get_mean_bal())
+    print(fruit.get_active_mean())
+    print(fruit.get_strawberry_mean())
+
+
+    result = doctest.testmod()
+    if result.failed == 0:
+        print("\nALL TESTS PASSED\n")
 
